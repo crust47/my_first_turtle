6da0378a9f4490ac1ae070c426e7f5eff652423c
crust47 <57227516+crust47@users.noreply.github.com>
2020-12-14 15:03:56 -0500

    first commit

---
+++ madlib.py
@@ -0,0 +1,15 @@
+# These are all the inputs you will fill in when you run the code
+h = input("What is your Favorite holiday? ")
+n = input("What is a noun you like? ")
+p = input("What is a place you would like to visit? ")
+t = input("list any persons name. ")
+a = input("Give me an adjective. ")
+b = input("List a body part in plural. ")
+v = input("List a verb. ")
+x = input("List another adjective. ")
+j = input("List another noun. ")
+f = input("what is your favorite food? ")
+m = input("List a plural noun. ")
+
+# This is what will come together in the end with all the strings combined
+print("I cant believe its already " + h + "!" " I cant wait to put on my " + n + " and visit every " + p + " in my neighborhood. This year, I am going to dress up as a " + t + " with " + a + " " + b + ". " " Before I " + v + " I make sure to grab my " + x + " " + j + " to hold all of my " + f + ". " " Finally, all my " + m + " are ready to go! ")
